6ae5e7a4f1ee6c19d83db073eb2d7477927938d7
paulomarinato <marinatopaulo@gmail.com>
2023-06-09 01:42:56 -0300

    config layout toolbar

--- app/src/main/java/com/marinato/leadsmeaning/activity/MainActivity.kt
+++ app/src/main/java/com/marinato/leadsmeaning/activity/MainActivity.kt
@@ -2,11 +2,21 @@ package com.marinato.leadsmeaning.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.marinato.leadsmeaning.R
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val app_toolbar = null
+        app_toolbar?.setupWithNavController(navController, appBarConfiguration)
+
     }
 }
